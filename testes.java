6ca34c96a61c43298b9f80d9d34f277d8512d348
Victor Cabral <110683578+Cabral137@users.noreply.github.com>
2023-09-25 18:40:20 -0300

    Update testes.java

--- testes.java
+++ testes.java
@@ -3,6 +3,27 @@ import java.util.Vector;
 
 public class Main
 {
+    
+        public static String getMenor (int [] array)
+        {
+            int menor = array[0];
+            int pos   = 0;
+            
+            for(int i = 0; i < array.length; i++)
+            {
+                if(menor > array[i])
+                {
+                    menor = array[i];
+                    pos   = i;
+                }
+            }
+            
+            String resp = menor + ";" + pos;
+            
+            return(resp);
+            
+        }
+    
         public static int [] ordenarMatriz(Vector<int[]> matriz)
         {
     
@@ -13,25 +34,36 @@ public class Main
             
             int [] ponteiros = new int [matriz.size()];
     
-            for(int j = 0; j < matriz.get(0).length; j++)
-            {
+            for(int j = 0; j < matriz.get(0).length * matriz.size(); j++)
+            {                
+                
                 for(int i = 0; i < matriz.size(); i++)
                 {
-                    tmp[i] = matriz.get(j)[ponteiros[i]];
+                    if(ponteiros[i] < matriz.get(0).length)
+                    {
+                        tmp[i] = matriz.get(i)[ponteiros[i]];
+                    }
+                    else
+                    {
+                        tmp[i] = 0xFFFFFFF;
+                    }
                     
-                    //System.out.println(ponteiros[i]);
                 }
     
-                for(int o = 1; o < tmp.length-1; o++)
-                {
-                    //System.out.println(tmp[o] + "---");
-                    
-                    if(tmp[o] < tmp[o-1])
-                    {
-                        resp[contador] = tmp[o];
-                        pos = o;
-                    }
     
+                for(int i = 0; i < tmp.length; i++)
+                {
+                    System.out.println(tmp[i]);
+                }
+                
+                System.out.println();
+    
+                for(int o = 0; o < tmp.length - 1; o++)
+                {
+                    String [] tmp2 = getMenor(tmp).split(";");
+                    
+                    resp[contador] = Integer.parseInt(tmp2[0]);
+                    pos = Integer.parseInt(tmp2[1]);
                 }
     
                 contador++;
@@ -54,11 +86,14 @@ public class Main
             int [] array2 = {2,3,5,6,7};
             matriz.add(array2);
             
+            int [] array3 = {2,3,5,6,7};
+            matriz.add(array3);
+            
             int [] print = ordenarMatriz(matriz);
             
             for(int i = 0; i < print.length; i++)
             {
-                System.out.println(print[i]);
+                System.out.print(print[i]);
             }
         }
 }
