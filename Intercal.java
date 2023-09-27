032812f55198c97f22066240391079336ddf80d5
Victor Cabral <110683578+Cabral137@users.noreply.github.com>
2023-09-27 19:49:27 -0300

    Update Intercal.java

--- Intercal.java
+++ Intercal.java
@@ -4,11 +4,13 @@ import java.util.Vector;
 public class Intercal 
 {
 
-    long filePointer;
+    long    filePointer;
+    long [] ponteirosCaminho;
 
     Intercal ()
     {
         this.filePointer = 0;
+        this.ponteirosCaminho = null;
     }
 
     public static void swap (Musica [] array, int i, int j)
@@ -56,7 +58,7 @@ public class Intercal
         quickSort(array, 0, array.length - 1);
     }
 
-    public Musica [] readArray (int registros)
+    public Musica [] readArray (int registros, String path)
     {
         Binario bin = new Binario();
 
@@ -65,7 +67,7 @@ public class Intercal
         
         try 
         {
-            RandomAccessFile ra = new RandomAccessFile("./SpotifyMusic.hex", "r");
+            RandomAccessFile ra = new RandomAccessFile(path, "r");
             ra.seek(filePointer);
             int contador = 0;
 
@@ -137,7 +139,7 @@ public class Intercal
             {
                 for(int i = 0; i < caminhos; i++)
                 {
-                    Musica [] array = readArray(registros);
+                    Musica [] array = readArray(registros, "./SpotifyMusic.hex");
 
                     quickSort (array);
                     String path = "./ARQUIVOS/caminho" + i + ".hex";
@@ -153,67 +155,174 @@ public class Intercal
 
     }
 
-    public Musica [] ordenarMatriz(Vector<Musica[]> matriz)
+    public static Musica getMenor (Musica [] array)
     {
-
-        Musica [] tmp  = new Musica [matriz.size()];
-        Musica [] resp = new Musica [(matriz.size() * matriz.get(0).length)]; // numero de registros * caminhos = numero total de registros
-        int contador = 0;
-        int pos = 0;
-        
-        int [] ponteiros = new int [matriz.size];
-
-        for(int j = 0; j < matriz.get(0).length; j++)
+        Musica menor = array[0];
+    
+        for(int i = 0; i < array.length; i++)
         {
-            for(int i = 0; i < matriz.size(); i++)
+            if(menor != null)
             {
-                tmp[i] = matriz.get(i)[ponteiros[j]];
-            }
-
-            for(int o = 1; o < tmp.length; o++)
-            {
-                if(tmp[o].getID() < tmp[o-1].getID())
+                if(array[i] != null)
                 {
-                    resp[contador] = tmp[o];
-                    pos = o;
+                    if(menor.getID() > array[i].getID())
+                    {
+                        menor = array[i];
+                    }
                 }
-
             }
-
-            contador++;
-            ponteiros[pos]++;
+            else
+            {
+                menor = array[i+1];
+            }
 
         }
+            
+        return(menor);
+            
+    }
 
+    public static int quantRegistros (Vector<Musica[]> matriz)
+    {
+        int quant = 0;
+
+        for(int i = 0; i < matriz.size(); i++)
+        {
+            quant = quant + matriz.get(i).length;
+        }
+
+        return(quant);
+
+    }
+
+    public static Musica [] ordenarMatriz(Vector<Musica[]> matriz, int registros, int caminhos)
+    {
+    
+        Musica [] tmp  = new Musica [caminhos];
+        Musica [] resp = new Musica [registros]; // caminhos * numero de registros = numero total de registros
+        int contador = 0;
+          
+        int [] ponteiros = new int [caminhos];
+
+        for(int j = 0; j < registros; j++)
+        {                
+
+            for(int i = 0; i < caminhos; i++)
+            {
+                if(ponteiros[i] < matriz.get(i).length)
+                {
+                    tmp[i] = matriz.get(i)[ponteiros[i]];
+                }
+                else
+                {
+                    tmp[i] = null;
+                }          
+            }
+ 
+            Musica tmp2 = getMenor(tmp);
+            int pos = 0;
+
+            for(int i = 0; i < tmp.length; i++)
+            {
+                if(tmp[i] != null)
+                {
+                    if(tmp2.getID() == tmp[i].getID())
+                    {
+                        pos  = i;
+                    }
+                }
+            }
+
+            resp[contador] = tmp2;
+            ponteiros[pos] = ponteiros[pos] + 1;
+    
+            contador++;
+    
+        }
+    
         return(resp);
+    
+    }
 
+    public Musica [] readCaminho (int registros, String path)
+    {
+        Binario bin = new Binario();
+
+        Musica [] array = new Musica [registros];
+        Musica music = new Musica ();
+        
+        try 
+        {
+            RandomAccessFile ra = new RandomAccessFile("./ARQUIVOS/" + path, "r");
+            ra.seek(ponteirosCaminho[Character.getNumericValue(path.charAt(7))]);
+            int contador = 0;
+
+            while(contador < registros)
+            {
+                boolean lapide = ra.readBoolean();
+                int tamanho = ra.readShort();
+                int id = ra.readShort();
+
+                byte [] musicArray = new byte [tamanho - 2];
+                ra.read(musicArray);
+                music = bin.fromByteArray(musicArray);
+                music.setID(id);
+                array[contador] = music;
+                contador++;
+            }
+
+            ponteirosCaminho[Character.getNumericValue(path.charAt(7))] = ra.getFilePointer();
+
+            ra.close();
+
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        
+
+        return (array);
     }
 
     public void balanceada (int registros, int caminhos)
     {
 
         primeiraParte (registros, caminhos);
-        
+
+        //INICIALIZANDO VETOR DE PONTEIROS
+        ponteirosCaminho = new long [caminhos];
+
+
         Vector<Musica[]> array = new Vector<Musica[]>(caminhos);
 
         try 
         {
-          
-            for(int i = 0; i < caminhos; i++)
+            for(int i = 0; i < 1; i++)
             {
-                RandomAccessFile ra = new RandomAccessFile("./ARQUIVOS/caminho" + i + ".hex", "rw");
-                Musica [] tmp = readArray(registros/caminhos);
 
-                array.add(tmp);
+                for(int j = 0; j < caminhos; j++)
+                {
+                    Musica [] tmp = readCaminho(registros/caminhos, "caminho" + j + ".hex");
+                    array.add(tmp);
+                }
 
+                Musica [] tmp = ordenarMatriz (array, registros, caminhos);
+
+                for(int j = 0; j < tmp.length; j++)
+                {
+                    System.out.println(tmp[j].getName() + " --- " + tmp[j].getID() + " --- " + tmp.length);
+                }
+
+                String path = "./ARQUIVOS/caminho" + (i + caminhos) + ".hex";
+                RandomAccessFile ra = new RandomAccessFile(path, "rw");
+                writeCaminho(path, tmp);
             }
-
-            Musica [] tmp = ordenarMatriz (array);
 
         } 
         catch (Exception e) 
         {
-            // TODO: handle exception
+            
         }
 
 
