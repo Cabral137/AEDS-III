89b585d4c8fd68f3f00b2cf88513eb969797c1b8
Victor Cabral <110683578+Cabral137@users.noreply.github.com>
2023-11-23 12:07:05 -0300

    Update KMP.java

--- TP02/KMP.java
+++ TP02/KMP.java
@@ -1,3 +1,4 @@
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class KMP
@@ -50,49 +51,92 @@ public class KMP
 		return(pos);
 	}
 
-	public void pesquisa ()
+	public static void pesquisa () 
 	{
-		
-		Scanner sc = new Scanner(System.in);
+		try 
+        {
+            Scanner sc = new Scanner(System.in);
 
-		System.out.print("\nNome do Arquivo: ");
-		String frase   = sc.nextLine();
+            System.out.print("\nNome do Arquivo: ");
+            String path   = sc.nextLine();
+            RandomAccessFile ra = new RandomAccessFile ("./" + path, "r");
 
-		System.out.println("\nFrase para pesquisar: ");
-		String palavra = sc.nextLine();
+            String frase = ra.readLine();
 
-		pesquisa(frase, palavra);
+            System.out.print("\nFrase para pesquisar: ");
+            String palavra = sc.nextLine();
+
+            pesquisa(frase, palavra);
+        } 
+        catch (Exception e) 
+        
+        {
+            System.out.println("ERRO: ARQUIVO NÃO ENCONTRADO");
+        }
+
 
 	}
 
-	public void pesquisa (String frase, String palavra)
+	public static void pesquisa (String frase, String palavra)
 	{
-		       
-		int [] pos = criarVetor(palavra);
-		int o = 0;
 
-		for(int i = 0; i < frase.length(); i++)
+		try 
 		{
+			Scanner sc = new Scanner(System.in);
+			boolean encontrou = false;   
 
-			if(palavra.charAt(o) == frase.charAt(i))
+			int [] pos = criarVetor(palavra);
+			int o = 0;
+
+			for(int i = 0; i < frase.length(); i++)
 			{
-				o++;
 
-				if(o == palavra.length())
+				if(palavra.charAt(o) == frase.charAt(i))
 				{
-					System.out.println("ACHOU");
-					break;
+					o++;
+
+					if(o == palavra.length())
+					{
+						System.out.println("\nACHOU");
+						System.out.println("Posição: " + i);
+						System.out.println("\n\nCONTINUAR? \n- SIM\n- NÃO");
+						encontrou = true;
+
+						if(!sc.nextLine().equals("SIM"))
+						{
+							break;
+						}
+
+						o = 0;
+						
+					}
+
+				}
+				else
+				{
+					i = i + (o - pos[o]);					
+					o = pos[o];
 				}
 
 			}
-			else
-			{
-				i = i + (o - pos[o]);					
-				o = pos[o];
-			}
 
+			if(!encontrou)
+			{
+				System.out.println("\nNÃO EXISTE ESSE PADRÃO NO ARQUIVO");
+			}
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("ERRO: ERRO NA PESQUISA");
+			e.printStackTrace();
 		}
 
+
+	}
+
+	public static void main(String[] args) 
+	{
+		pesquisa();
 	}
 
 }
