a0b2964d65677574fe3c797fe4cdb5595c93b8ad
Victor Cabral <110683578+Cabral137@users.noreply.github.com>
2023-11-25 20:46:19 -0300

    Update BoyerMoore.java

--- TP02/BoyerMoore.java
+++ TP02/BoyerMoore.java
@@ -1,23 +1,27 @@
 import java.io.RandomAccessFile;
 import java.util.Scanner;
+import java.util.Vector;
 
+// Classe para gerenciar a tabela de hash utilizada no algoritmo Boyer-Moore
 class Hash
 {
-    public int  [] pos;
+    public int  [] pos; // Variável para armazenar a posição para cada caso de comparação
 
+    // Construtor da classe Hash
     public Hash ()
     {
         this.pos = new int  [256];
     }
 
+    // Obtém a posição de um caractere na tabela de hash
     public int getPos(char search)
     {
         return(this.pos[search % 256]);
     }
 
+    // Inicializa a tabela de hash para o bad character rule
     public void badChar (String frase)
     {
-
         for(int i = 0; i < pos.length; i++)
         {
             this.pos[i] = -1;
@@ -27,84 +31,130 @@ class Hash
         {
             pos[frase.charAt(i) % 256] = i;
         }
-
     }
-
 }
 
+// Classe principal que implementa o algoritmo Boyer-Moore
 public class BoyerMoore 
 {
-
+    // Método para interagir com o usuário e iniciar a pesquisa
 	public void pesquisa () 
 	{
 		try 
         {
             Scanner sc = new Scanner(System.in);
 
+            // Solicitar o nome do arquivo
             System.out.print("\nNome do Arquivo: ");
             String path   = sc.nextLine();
-            RandomAccessFile ra = new RandomAccessFile ("./" + path, "r");
+            RandomAccessFile ra = new RandomAccessFile ("./ARQUIVOS/TESTE/" + path, "r");
 
-            String frase = ra.readLine();
+            String frase = "";
+            while (ra.getFilePointer() != ra.length())
+            {
+                frase = frase + ra.readLine();
+            }
 
-            System.out.print("\nFrase para pesquisar: ");
+            // Solicitar a palavra a ser pesquisada
+            System.out.print("\nPalavra para pesquisar: ");
             String palavra = sc.nextLine();
 
+            // Iniciar a pesquisa
             pesquisa(frase, palavra);
         } 
         catch (Exception e) 
-        
         {
             System.out.println("ERRO: ARQUIVO NÃO ENCONTRADO");
         }
-
-
 	}
 
+    // Função auxiliar para retornar o valor máximo entre dois números
+    public static int max (int num1, int num2)
+    {
+        if(num1 > num2)
+        {
+            return(num1);
+        }
+
+        return(num2);
+    }
+
+    // Método principal para realizar a pesquisa Boyer-Moore
     public static void pesquisa (String frase, String palavra)
     {
-
-        Hash tabela = new Hash();
-        
-        tabela.badChar(palavra);
-
-        int o = palavra.length() - 1;
-        int i = palavra.length() - 1;
-            
-        while(i < frase.length())
+        try 
         {
+            Hash tabela = new Hash();
+            Scanner sc = new Scanner(System.in);
 
-            if(palavra.charAt(o) == frase.charAt(i))
+            // Inicializar a tabela de hash
+            tabela.badChar(palavra);
+
+            int o = palavra.length() - 1;
+            int i = palavra.length() - 1;
+            int encontrou   = 0;
+            int comparacoes = 0;
+
+            while(i < frase.length())
             {
-                o--;
-                i--;
+                comparacoes++;
 
-                if(o == 1)
+                if(palavra.charAt(o) == frase.charAt(i))
                 {
-                    System.out.println("ACHOU");
-                    break;
-                }
+                    o--;
+                    i--;
 
-            }
-            else
-            {
-                int tmp = tabela.getPos(frase.charAt(i));
+                    if(o == 1)
+                    {
+                        System.out.println("\n\nACHOU");
+                        System.out.println("Posição: " + (i - 2));
+                        System.out.println("Comparações: " + comparacoes);
+                        System.out.println("\n\nCONTINUAR? \n- SIM\n- NÃO");
+                        encontrou++;
 
-                if(tmp < 0)
-                {
-                    i = i + palavra.length();
-                    o = palavra.length() - 1;
+                        if(sc.nextLine().equals("SIM"))
+                        {
+                            i = i + palavra.length();
+                            o = palavra.length() - 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
                 }
                 else
                 {
-                    i = i + (o - tabela.getPos(frase.charAt(i)));
-                    o = palavra.length() - 1;
+                    if((i+o) <= frase.length())
+                    {
+                        i = i + max(1, o - tabela.getPos(frase.charAt(i+o)));
+                    } 
                 }
-
             }
 
-        }
+            // Exibir resultados da pesquisa
+            if(encontrou < 1)
+			{
+				System.out.println("\nNÃO EXISTE ESSE PADRÃO NO ARQUIVO");
+			}
+			else
+			{
+				if(encontrou == 1)
+				{
+					System.out.println("\nO PADRÃO FOI ENCONTRADO " + encontrou + " VEZ NO ARQUIVO");
+				}
+				else
+				{
+					System.out.println("\nO PADRÃO FOI ENCONTRADO " + encontrou + " VEZES NO ARQUIVO");
+				}
+			}
 
+            sc.nextLine();
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("\nERRO: ERRO NA PESQUISA");
+			e.printStackTrace();
+        }
     }
-    
 }
